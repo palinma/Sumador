5ac031c6e982e725003bf9ca26f5dfd9b54076fb
Iván <ivigalassi2003@gmail.com>
2025-03-15 19:17:02 +0100

    Primer commit del proyecto

---
+++ src/main/java/com/mycompany/actividadrepaso/NewMain.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.actividadrepaso;
+
+/**
+ *
+ * @author iviva
+ */
+public class NewMain {
+    public static void main(String[] args) {
+System.out.println(Sumador.sumar (15f,4f));
+}}
---
+++ src/main/java/com/mycompany/actividadrepaso/Sumador.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividadrepaso;
+
+/**
+ *
+ * @author iviva
+ */
+public class Sumador {
+    public static float sumar (float a, float b ) {
+return a + b;
+}}
